6bd282a58eb204b17e67b7af76f59d66ec374f8a
GonzalD <d.gonzalez@outlook.fr>
2016-12-09 17:06:22 +0100

    get du service à jour (encapsulé)

--- www/js/http.service.js
+++ www/js/http.service.js
@@ -2,45 +2,40 @@
 
   angular.module('http.services', [])
 
-  .factory('milkProducts', milkService);
+  .factory('milkService', milkService);
 
   milkService.$inject = ['$http'];
 
   function milkService($http) {
 
     var service = {
-      get: get,
+      get: getMilk,
       configure: configure
     };
 
-    var url = 'http://api.karibou.evaletolab.ch/v1/products/category/produits-laitiers';
+    var URL = 'http://api.karibou.evaletolab.ch/v1/products/category/produits-laitiers';
     var useHTTP = configure;
 
     return service;
 
       //récupère donnée API depuis le site ou un fichier local en fonction de la config
-      function get(){
-        var got;
-        if(useHTTP){
-          got = $http.get(url);
-        }
-        else {
-          got = $http.get('produits_laits.json');
-        }
-          got
-            .then(getComplete)
-            .catch(getFailed);
+      function getMilk(){
+        var url=(useHTTP)?URL:'produits_laits.json';
 
+        var promise=$http.get(url).then(getComplete)
+           .catch(getFailed);
 
         function getComplete(response) {
-          return response.data.results;
+          return response.data;
         }
 
         function getFailed(error) {
             logger.error('XHR Failed for getMilk.' + error.data);
         }
 
+        return promise;
       }
+
 
       function configure(fromAPI){
         return fromAPI;
