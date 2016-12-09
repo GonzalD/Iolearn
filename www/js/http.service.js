5d842e0c55128c9d237ab2afff096a91c8bbe457
GonzalD <d.gonzalez@outlook.fr>
2016-12-09 15:06:27 +0100

    centralisation des fonctions get milk dans le service

--- www/js/http.service.js
+++ www/js/http.service.js
@@ -2,27 +2,48 @@
 
   angular.module('http.services', [])
 
-  .factory('ProductsServ', getProducts);
+  .factory('milkProducts', milkService);
 
-  getProducts.$inject = ['$http'];
+  milkService.$inject = ['$http'];
 
-  function getProducts($http) {
+  function milkService($http) {
 
     var service = {
-      getJson: getJson,
-      getFile: getFile
+      get: get,
+      configure: configure
     };
+
+    var url = 'http://api.karibou.evaletolab.ch/v1/products/category/produits-laitiers';
+    var useHTTP = configure;
 
     return service;
 
-      //récupère donnée API depuis le site
-      function getJson(url){
-        return $http.get(url);
+      //récupère donnée API depuis le site ou un fichier local en fonction de la config
+      function get(){
+        var got;
+        if(useHTTP){
+          got = $http.get(url);
+        }
+        else {
+          got = $http.get('produits_laits.json');
+        }
+          got
+            .then(getComplete)
+            .catch(getFailed);
+
+
+        function getComplete(response) {
+          return response.data.results;
+        }
+
+        function getFailed(error) {
+            logger.error('XHR Failed for getMilk.' + error.data);
+        }
+
       }
 
-      //donnée API depuis fichier (produits_laits.json dans racine)
-      function getFile(){
-        return $http.get('produits_laits.json');
+      function configure(fromAPI){
+        return fromAPI;
       }
 
   }
