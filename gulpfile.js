9a9cd85c70d4c3d7eef1f75d615717ecd3a6fdb9
GonzalD <d.gonzalez@outlook.fr>
2017-01-12 16:55:36 +0100

    tâches gulp de minification et compression fichiers à mettre dans prod/

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,15 @@ var sass = require('gulp-sass');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
+var uglify = require('gulp-uglify');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
 
 var paths = {
   sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass']);
+gulp.task('default', ['sass', 'cpy-to-prod','js-min', 'css-min', 'html-min', 'image-min']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -22,7 +25,7 @@ gulp.task('sass', function(done) {
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./prod/css/'))
     .on('end', done);
 });
 
@@ -48,4 +51,44 @@ gulp.task('git-check', function(done) {
     process.exit(1);
   }
   done();
+});
+
+gulp.task('js-min', function() {
+  gulp.src([
+    ' ./www/**/*.js'
+  ], {base: './www/'})
+  .pipe(uglify())
+  .pipe(gulp.dest('./prod/'));
+});
+
+gulp.task('css-min', function() {
+  gulp.src('./www/css/*.css')
+  .pipe(minifyCss({
+    keepSpecialComments: 0
+  }))
+  .pipe(rename({ extname: '.min.css' }))
+  .pipe(gulp.dest('./prod/css'));
+});
+
+gulp.task('html-min', function() {
+  gulp.src([
+    './www/templates/*.html',
+    './www/index.html'
+  ], {base: './www/'})
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest('./prod/'));
+});
+
+gulp.task('image-min', function(){
+  gulp.src('www/img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('./prod/img/'));
+});
+
+gulp.task('cpy-to-prod', function(){
+  gulp.src([
+    './www/lib/*',
+    './www/*.json'
+  ], {base: './www/'})
+  .pipe(gulp.dest('./prod/'));
 });
